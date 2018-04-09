f28f134f529cb5dc880b01529e61c90369cdd1b8
Rafael Cazuriaga <rafael.cazuriaga@akqa.com>
2018-04-09 17:07:02 +1200

    Async in Redux: Dispatch Action on Page Load

--- src/index.js
+++ src/index.js
@@ -7,8 +7,10 @@ import routes from './routes';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { Router, browserHistory } from 'react-router/lib';
+import {loadCourses} from './actions/courseActions';
 
 const store = configureStore();
+store.dispatch(loadCourses());
 render(
   <Provider store={store}>
     <Router history={browserHistory} routes={routes} />
